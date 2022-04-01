bbc08ee5d4fa2414d5abc44d4147d1738a8239c7
ISSAO TORIBIO TAKEUCHI <issaottakeuchi@gmail.com>
2022-04-01 20:42:17 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ fetch('https://randomuser.me/api/')
     })
     .then(data => {
         //manipulamos a resposta
+        const dados = data.results
+        console.log(dados);
+        dados.forEach(dados => renderizarDadosUsuario(dados));
+
+
         console.log(data)
     });
 
@@ -13,6 +18,32 @@ function renderizarDadosUsuario(dados) {
     // Aqui devem desenvolver uma função que seja exibida na tela:
     // a foto, o nome completo do usuário e o e-mail.
     // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
+
+    const card = document.querySelector(".card")
+    card.style.textAlign = 'center'
+    card.style.padding = '12%'
+    card.style.display = "flex"
+    card.style.flexDirection = 'column'
+    card.style.justifyContent = 'center'
+    card.style.alignItems = 'center'
+    card.style.gap = '20%'
+
+    const picture = document.createElement("img")
+    picture.src = dados.picture.large;
+
+    const name = document.createElement("h1")
+    name.innerText = dados.name.first + ' ' + dados.name.last;
+
+    const email = document.createElement("h2")
+    email.innerText = dados.email;
+
+ 
+    card.appendChild(picture)
+    card.appendChild(name)
+    card.appendChild(email)
+    console.log(card);
+
+
 }
 
 
